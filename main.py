4526a3fc32f86075a919e002990fa27c854b51d6
kyoma <zunsure2@gmail.com>
2024-06-07 21:01:52 +0200

    First layer

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-
+import subprocess
 import os
 from simple_term_menu import TerminalMenu
 
 class Ninja:
     def __init__(self):
-        pass
+        self.red = '\033[1;31m'
+        self.green = '\033[32m'
+        self.reset = '\033[0m'
 
     def troubleshoot(self):
         print("You selected Troubleshoot.")
-        # Add Troubleshoot logic here
+        print("\n-----------------------------------")
+        print("Troubleshooting first layer ISO/OSI"
+            "\n(Your Network interfaces)\n")
+        try:
+            result = subprocess.run(["ip", "-br", "a"], text = True, capture_output= True)
+            lines = result.stdout.strip().split('\n')
+            interfaces = []
+            for line in lines:
+                parts = line.split()
+                interface = {
+                    'name': parts[0],
+                    'status': parts[1],
+                    'address': parts[2:]
+           }        
+                interfaces.append(interface)
+                addresses = ', '.join(interface['address'])
+                if interface['status'] == 'DOWN':
+                    print(f"{self.red}UH-OH, This interface is not up {self.reset}\n")
+                    print(f"[+] NAME: {interface['name']}, \n[+] STATUS: {self.red}{interface['status']}{self.reset}, \n[+] ADDRESSES: {addresses}\n")
+
+                elif interface["status"] == 'UP':
+                    print(f"[+] NAME: {interface['name']}, \n[+] STATUS: {self.green}{interface['status']}{self.reset}, \n[+] ADDRESSES: {addresses}\n")
+                    print(f"{self.green}UP AND RUNNING {self.reset}\n")
+
+                else:
+                    print(f"[+] NAME: {interface['name']}, \n[+] STATUS: {self.green}{interface['status']}{self.reset}, \n[+] ADDRESSES: {addresses}\n")
+                    print(f"UNKNOWN STATUS\n")
+
+        except Exception as e:
+            print(f"ERR:,{e} ")
+        
+        print("------------------------------------")
+        
         input("Press Enter to continue...")
 
     def network_health_status(self):
